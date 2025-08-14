e05d804fbd5645ee5423338bdf0aa1f232681984
trizDamacena <beatrizfdamacena@gmail.com>
2025-08-14 16:03:52 -0300

    api_view

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('Autores', AutoresView.as_view())
+    path('Autores', AutoresView.as_view()),
+    path('Template', visualizacao)
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,30 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import Autor
 from .serializers import AutorSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class AutoresView(ListCreateAPIView):
-    queryset = Autor.objects.all()
+    queryset = Autor.objects.all() 
     serializer_class = AutorSerializers
+
+def visualizacao(request):
+    autores = Autor.objects.all()
+    return render(request,"todo/index.html", {"autores": autores})
+
+@api_view(['GET', 'POST'])
+def list_autor(request):
+    if request.method == 'GET':
+        queryset = Autor.objects.all()
+        serializer = AutorSerializers(queryset, many = True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = AutorSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
