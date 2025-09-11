c3d633a0cc6a73662bbcb5d6998d119931897f1b
trizDamacena <beatrizfdamacena@gmail.com>
2025-09-11 16:55:15 -0300

    Population 2

--- api/models.py
+++ api/models.py
@@ -11,15 +11,15 @@ class Autor(models.Model):
         return f"{self.nome} {self.sobrenome}"
 
 class Editora(models.Model):
-    nome_editora = models.CharField(max_length=255)
+    editora = models.CharField(max_length=255)
     cnpj = models.CharField(max_length=18, unique=True, null=True, blank=True)
-    endereco_editora = models.CharField(max_length=200, null=True, blank=True)
-    telefone_editora = models.CharField(max_length=20, null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     site = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.nome_editora}"
+        return f"{self.editora}"
     
 
 class Livro(models.Model):
