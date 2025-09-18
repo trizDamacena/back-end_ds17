4738dc9f8f13d52f6d5467d579f51f5fd9caa74a
trizDamacena <beatrizfdamacena@gmail.com>
2025-09-18 16:58:01 -0300

    18/09

---
+++ front/src/app/models/autor.ts
@@ -0,0 +1,9 @@
+export interface Autor{
+    id: number;
+    nome: string;
+    sobrenome: string;
+    data_nasc ?: string | null;
+    nacion?: string | null;
+    biogra?: string | null;
+}
+
---
+++ front/src/app/services/autores.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Autor } from "../models/autor";
+import { environment } from "../environments/environment";
+
+@Injectable({providedIn: 'root'})
+export class AutoresService{
+    private http = inject(HttpClient)
+    private base = environment.apiBase
+
+    listar(): Observable<Autor[]>{
+        const url = `${this.base}Autores`
+        return this.http.get<Autor[]>(url)
+    }
+}
